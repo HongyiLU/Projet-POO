994191a0b473b403625377d31d55a098f6002636
HongyiLU <adamlu389535891@gmail.com>
2019-04-11 04:19:33 +0200

    Prototype

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/51924/CLionProjects/Projet-POO
+# Install script for directory: C:/Users/HONGYI LU/Documents/CLionProjects/Projet-POO
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/51924/CLionProjects/Projet-POO/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/HONGYI LU/Documents/CLionProjects/Projet-POO/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
