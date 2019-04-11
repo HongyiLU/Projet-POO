29503699fb98666d9101080690076e78a9595546
JohnzXia030 <47855436+JohnzXia030@users.noreply.github.com>
2019-04-11 08:51:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,5 +8,6 @@ int main() {
     a1.AddBien();
     a1.AddPropoAchat(1,464646);
     a1.AddAchatEff();
+    //veuillez bien saisir le path absolu dans la methode ReadInfo pour tester la fonction
     a1.ReadInfo("C:\\Users\\51924\\CLionProjects\\Projet-POO\\biens.txt");
 }
